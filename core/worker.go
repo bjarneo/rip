ed549af6e2934a31d9075009a90229496e46add2
Bjarne Øverli <bjarne.oeverli@gmail.com>
2022-03-23 08:00:06 +0100

    refactor: move worker to a function called execute

---
+++ core/worker.go
@@ -0,0 +1,67 @@
+package core
+
+import (
+	"sync"
+	"time"
+
+	"github.com/pterm/pterm"
+)
+
+// Initialize the cli arguments
+var args Arguments = NewArgs()
+
+// Initialize our statistics
+var stats TotalStatistics = NewStatistics()
+
+func Execute() {
+	// Let us start the timer for how long the workers are running
+	start := NowUnixMilli()
+	end := FutureUnixMilli(args.Interval())
+
+	ParseHeaders(args)
+
+	area, _ := pterm.DefaultArea.Start()
+
+	var wg sync.WaitGroup
+
+	// Start the wait groups
+	for i := 0; i < args.Concurrent(); i++ {
+		wg.Add(1)
+
+		Request(args.Hosts(), args, &stats)
+	}
+
+	// This loop will run until the end is reached
+	// then it will close the wait groups and break the loop
+	for {
+		// Run the for loop once a second
+		time.Sleep(time.Second * time.Duration(1))
+
+		area.Update(Logo() + PrintStats(&stats))
+
+		if NowUnixMilli() < end {
+			continue
+		}
+
+		// Close all the concurrent wait groups
+		for i := 0; i < args.Concurrent(); i++ {
+			wg.Done()
+		}
+
+		break
+	}
+
+	// Block until wait groups has been closed
+	wg.Wait()
+
+	// End the timer for how long the workers are running
+	stop := NowUnixMilli()
+
+	stats.SetElapsedTime(stop - start)
+
+	// Final update
+	area.Update(Logo() + PrintStats(&stats))
+
+	// stop the area update
+	area.Stop()
+}
--- main.go
+++ main.go
@@ -1,71 +1,8 @@
 package main
 
 import (
-	"sync"
-	"time"
-
 	"github.com/bjarneo/rip/core"
-	"github.com/pterm/pterm"
 )
-
-// Initialize the cli arguments
-var args core.Arguments = core.NewArgs()
-
-// Initialize our statistics
-var stats core.TotalStatistics = core.NewStatistics()
-
-func workers(concurrent int, interval int, hosts []string) {
-	// Let us start the timer for how long the workers are running
-	start := core.NowUnixMilli()
-	end := core.FutureUnixMilli(interval)
-
-	core.ParseHeaders(args)
-
-	area, _ := pterm.DefaultArea.Start()
-
-	var wg sync.WaitGroup
-
-	// Start the wait groups
-	for i := 0; i < concurrent; i++ {
-		wg.Add(1)
-
-		core.Request(hosts, args, &stats)
-	}
-
-	// This loop will run until the end is reached
-	// then it will close the wait groups and break the loop
-	for {
-		// Run the for loop once a second
-		time.Sleep(time.Second * time.Duration(1))
-
-		area.Update(core.Logo() + core.PrintStats(&stats))
-
-		if core.NowUnixMilli() < end {
-			continue
-		}
-
-		// Close all the concurrent wait groups
-		for i := 0; i < concurrent; i++ {
-			wg.Done()
-		}
-
-		break
-	}
-
-	// Block until wait groups has been closed
-	wg.Wait()
-
-	// End the timer for how long the workers are running
-	stop := core.NowUnixMilli()
-
-	stats.SetElapsedTime(stop - start)
-
-	// Final update
-	area.Update(core.Logo() + core.PrintStats(&stats))
-
-	// stop the area update
-	area.Stop()
-}
 
 func main() {
 	// Initialize the logger
@@ -73,5 +10,5 @@ func main() {
 	defer logger.Close()
 
 	// Run until the interval is done
-	workers(args.Concurrent(), args.Interval(), args.Hosts())
+	core.Execute()
 }
